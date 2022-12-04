a19ceddd5c78fc7cbbaded53a5aedaf693cfed1c
Asravin <119303642+Asravin@users.noreply.github.com>
2022-12-04 18:44:33 +0300

    Update Program.cs

--- question_2/Program.cs
+++ question_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите "программу, которая принимает на вход три числа и выдает максимальное из этих чисел."
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Ведите число:");
+int a=Convert.ToInt32(Console.ReadLine());
+int b=Convert.ToInt32(Console.ReadLine());
+int c=Convert.ToInt32(Console.ReadLine());
+  
+if(a>b && a>c)
+{
+    Console.Write(a);
+}
+
+if(b>a && a>c)
+{
+    Console.Write(b);
+}
+
+if(c>a && c>b)
+{
+    Console.Write(c);
+}
